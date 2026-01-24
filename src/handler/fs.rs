f88ee7d0932da78162c47d26fd8d6a8d5ef67fbd
zack <qingyuehanxi@gmail.com>
2026-01-24 16:32:55 +0800

    v5

--- src/handler/fs.rs
+++ src/handler/fs.rs
@@ -3,7 +3,10 @@ use crate::{
     model::{
         dto::fs::FsRoot,
         param::fs::{FsLsParam, FsMkdirParam, FsMvParam, FsRmParam, FsTouchParam},
-        result::{base::BizResult, fs::FsLsResult},
+        result::{
+            base::BizResult,
+            fs::{FsItem, FsKind, FsLsResult},
+        },
     },
 };
 use axum::{Extension, Json, response::IntoResponse};
@@ -11,42 +14,37 @@ use walkdir::WalkDir;
 
 pub struct FsHandler;
 impl FsHandler {
-    pub async fn ls0(Json(_param): Json<FsLsParam>) -> BizResult<FsLsResult> {
-        let d = FsLsResult {
-            path: vec![
-                String::from("Document"),
-                String::from("Download"),
-                String::from("Music"),
-                String::from("Video"),
-            ],
-        };
-        BizResult::ok(d)
-    }
-
     pub async fn ls(
         Extension(fs_root): Extension<FsRoot>,
         Json(param): Json<FsLsParam>,
     ) -> Result<BizResult<FsLsResult>, BizError> {
         let p = param.path.trim_start_matches('/').to_string();
-        let fs_root = fs_root.path.clone();
-
-        let entries: Result<Vec<String>, BizError> = tokio::task::spawn_blocking(move || {
-            let dst = fs_root.join(p);
+        let fs_root_path = fs_root.path.clone();
+        let raw_param_path = param.path.clone();
+        let entries: Result<Vec<FsItem>, BizError> = tokio::task::spawn_blocking(move || {
+            let dst = fs_root_path.join(p);
             let dst = std::fs::canonicalize(&dst)
-                .map_err(|_| BizError::FILE_IO.concat_msg(format!("path not found: {}", param.path)))?;
-            if !dst.starts_with(&fs_root) {
+                .map_err(|_| BizError::FILE_IO.concat_msg(format!("path not found: {}", raw_param_path)))?;
+            if !dst.starts_with(&fs_root_path) {
                 return Err(BizError::FILE_IO.concat_msg("illegal path"));
             }
             if !dst.is_dir() {
                 return Err(BizError::FILE_IO.concat_msg("isn't directory"));
             }
-            let mut result = Vec::<String>::new();
+            let mut result = Vec::<FsItem>::new();
             let walker = WalkDir::new(dst).min_depth(1).max_depth(1).sort_by_file_name();
             for entry in walker {
                 match entry {
                     Ok(e) => {
-                        let name = e.file_name().to_string_lossy().to_string();
-                        result.push(name);
+                        if let Ok(metadata) = e.metadata() {
+                            let name = e.file_name().to_string_lossy().to_string();
+                            let kind = if metadata.is_dir() { FsKind::Dir } else { FsKind::File };
+                            result.push(FsItem {
+                                kind,
+                                name,
+                                size: metadata.len(),
+                            });
+                        }
                     }
                     Err(_) => continue,
                 }
@@ -69,6 +67,7 @@ impl FsHandler {
     pub async fn mv(Json(_param): Json<FsMvParam>) -> Result<impl IntoResponse, BizError> {
         Ok(BizResult::ok(()))
     }
+
     pub async fn rm(Json(_param): Json<FsRmParam>) -> Result<impl IntoResponse, BizError> {
         Ok(BizResult::ok(()))
     }
--- src/model/result/fs.rs
+++ src/model/result/fs.rs
@@ -1,6 +1,20 @@
 use serde::{Deserialize, Serialize};
 
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum FsKind {
+    Dir,
+    File,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct FsItem {
+    pub kind: FsKind,
+    pub name: String,
+    pub size: u64,
+}
+
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct FsLsResult {
-    pub path: Vec<String>,
+    pub path: Vec<FsItem>,
 }
